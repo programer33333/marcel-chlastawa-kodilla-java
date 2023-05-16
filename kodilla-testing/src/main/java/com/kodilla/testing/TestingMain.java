6c751c09e33061722301c47753a9ed2ae1cdb733
Marcel <chlastawamarcel@gmail.com>
2023-05-16 08:16:03 +0200

    6.2 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator newCalculator = new Calculator(8,7);
+
+        int addMethod = newCalculator.add();
+
+        if (addMethod == (newCalculator.getA() + newCalculator.getB())) {
+            System.out.println("Test Add method ok");
+        } else {
+            System.out.println("Add Method Error!");
+        }
+
+        int substractMethod = newCalculator.subtract();
+
+        if (substractMethod == (newCalculator.getA() - newCalculator.getB())) {
+            System.out.println("Test substract method ok");
+        } else {
+            System.out.println("Subastract method Error!");
+        }
+
     }
 }
+
+
