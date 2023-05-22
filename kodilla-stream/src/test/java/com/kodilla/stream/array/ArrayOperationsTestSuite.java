17a003e5985b0b159296bb1ff2ceb6d8051c8b18
Marcel <chlastawamarcel@gmail.com>
2023-05-22 23:03:05 +0200

    7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        //When
+
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+        assertEquals(10.5, average);
+    }
+}
